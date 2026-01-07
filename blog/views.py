d341b9e12da5974a7c4ba0018fd58282302d66ff
deme <Demetre.gochelashvili@gmail.com>
2026-01-07 17:32:57 +0400

    users app

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 5, 2026',
-    },
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'Second post content',
-        'date_posted': 'January 6, 2026',
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
